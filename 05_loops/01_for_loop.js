80f84bb75244eeff83058bec847c8712e10e9ecb
niteshim0 <niteshim111@gmail.com>
2024-02-20 08:06:06 +0530

    for loop in js

---
+++ 05_loops/01_for_loop.js
@@ -0,0 +1,46 @@
+for (let i = 0; i <= 10; i++) {
+  const element = i;
+  if (element == 5) {
+      console.log("5 Detected");
+  }
+  console.log(element);
+  
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Outer loop value: ${i}`);
+ for (let j = 1; j <= 10; j++) {
+  console.log(`Inner loop value ${j} and Outer loop ${i}`);
+  console.log(i + '*' + j + ' = ' + i*j );
+ }
+  
+}
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+  
+}
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break // it makes you move out of the whole loop // no further loop exectution
+    }
+   console.log(`Value of i is ${index}`);
+  
+}
+
+for (let index = 1; index <= 20; index++) {
+  if (index == 5) {
+      console.log(`Detected 5`);
+      //it will only skips the execution after this for that index value
+      continue
+  }
+ console.log(`Value of i is ${index}`);
+  
+}
