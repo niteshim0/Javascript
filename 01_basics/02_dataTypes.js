914abc1ad88ea1b4d83531ea648843e597cea226
Nitesh Kushwaha <niteshim111@gmail.com>
2024-07-09 11:23:27 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -28,13 +28,13 @@ console.log(typeof null); //object
  
 2.String: Represents a sequence of characters enclosed in single or double quotes. Example: "Hello, world!".
 
-3,Boolean: Represents a binary value, either true or false, often used for conditional logic.
+3.Boolean: Represents a binary value, either true or false, often used for conditional logic.
 
 4.Undefined: Represents a variable that has been declared but hasn't been assigned a value yet. Example: let x;.
 
 5.Null:  standalone value it is represenataion of empty value it is special type and it is a object type. Example: let y = null; 
 
-6.Symbol (ES6): it is mostly used used to find uniqness.
+6.Symbol (ES6): it is mostly used used to find uniqueness.
 
 7.BigInt (ES11): Represents large integers that cannot be represented by the Number type.
 
@@ -99,3 +99,6 @@ lets talk about some Range of Primitive DataType:
        Arrays  =>  object
        Function  =>  function
        Object  =>  object   */
+
+
+// Almost everything is an Object in Javascript except Primitive tYPES
