b232255b04877cab2e57ec8ee7a6f48840f45e5e
Nitesh Kushwaha <niteshim111@gmail.com>
2024-07-09 11:26:31 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,16 +1,16 @@
 //only unpredictable ones
 
-console.log("2" > 1);
-console.log("02">1)
+console.log("2" > 1); // true
+console.log("02">1)  // true
 
-console.log(null>0)
-console.log(null == 0)
-console.log(null>=0)
+console.log(null>0) // false
+console.log(null == 0) // false
+console.log(null>=0) // true
 
-console.log(undefined>0)
-console.log(undefined == 0)
-console.log(undefined>=0)
+console.log(undefined>0) // false
+console.log(undefined == 0) // false
+console.log(undefined>=0)   // false
 
-console.log("2" === 2)
-console.log("2" == 2)
+console.log("2" === 2) // false
+console.log("2" == 2) // true
 
