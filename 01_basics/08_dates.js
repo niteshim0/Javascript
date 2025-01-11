f0710c0e3cefd4f0ccc85a701d74a26488f48170
Nitesh Kushwaha <niteshim111@gmail.com>
2025-01-11 13:20:19 +0530

    Update 08_dates.js

--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -2,7 +2,7 @@ const date = new Date();
 console.log(Date)//[Function: Date]
 console.log(date);//2024-02-09T14:31:55.024Z
 
-//handling Data is major pain in ECMAScript a new Object Temporal has been proposed to get rid of this
+//handling Date is major pain in ECMAScript a new Object Temporal has been proposed to get rid of this
 //may be in future this file will be of no use but who have seen tommorow so we need to learn it
 //month starts from 0 not 1
 console.log(date.toDateString())//Returns a date as a string value.
